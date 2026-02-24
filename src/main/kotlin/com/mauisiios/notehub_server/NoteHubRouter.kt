85dd9bd44f8b7ef61a143247823e4ddf67f77e5a
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-02-24 02:00:44 -0500

    Add `NoteHandler` and `NoteHubRouter` for note management APIs

---
+++ src/main/kotlin/com/mauisiios/notehub_server/NoteHubRouter.kt
@@ -0,0 +1,32 @@
+package com.mauisiios.notehub_server
+
+import com.mauisiios.notehub_server.handler.NoteHandler
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
+import org.springframework.web.reactive.function.server.coRouter
+
+@Configuration(proxyBeanMethods = false)
+class NoteHubRouter() {
+
+    @Bean
+    fun notesRoutes(
+        noteHandler: NoteHandler
+    ): RouterFunction<ServerResponse> = coRouter {
+        "/note".nest {
+            GET("/title/{title}", noteHandler::getNoteByTitle)
+            "/{id}".nest {
+                GET("", noteHandler::getNote)
+                DELETE("", noteHandler::deleteNote)
+            }
+            accept(MediaType.APPLICATION_JSON).nest {
+                POST("", noteHandler::createNote)
+                PATCH("", noteHandler::updateNote)
+            }
+            GET("", noteHandler::listNotes)
+        }
+    }
+}
---
+++ src/main/kotlin/com/mauisiios/notehub_server/handler/NoteHandler.kt
@@ -0,0 +1,86 @@
+package com.mauisiios.notehub_server.handler
+
+import com.mauisiios.notehub_server.dto.NoteDto
+import com.mauisiios.notehub_server.service.NoteService
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.server.ServerRequest
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.awaitBodyOrNull
+import org.springframework.web.reactive.function.server.bodyAndAwait
+import org.springframework.web.reactive.function.server.bodyValueAndAwait
+import org.springframework.web.reactive.function.server.buildAndAwait
+import kotlin.apply
+
+@Component
+class NoteHandler(
+    private val noteService: NoteService
+) {
+    suspend fun listNotes(request: ServerRequest): ServerResponse = coroutineScope {
+        val notes = noteService.getAll()
+
+        ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyAndAwait(notes)
+    }
+
+    suspend fun getNote(request: ServerRequest): ServerResponse = coroutineScope {
+        val id = request.pathVariable("id").toLong()
+        val note = noteService.getById(id)
+            ?: return@coroutineScope ServerResponse.notFound().buildAndAwait()
+
+        ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValueAndAwait(note)
+    }
+
+    /**
+     * handler for getting a note by title.
+     * title must be a path variable
+     */
+    suspend fun getNoteByTitle(request: ServerRequest): ServerResponse = coroutineScope {
+        val title = request.pathVariable("title")
+        val note = noteService.getByTitle(title)
+            ?: return@coroutineScope ServerResponse.notFound().buildAndAwait()
+
+        ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValueAndAwait(note)
+    }
+
+    suspend fun createNote(request: ServerRequest): ServerResponse = coroutineScope {
+        val note = request.awaitBodyOrNull<NoteDto>()
+            ?: return@coroutineScope ServerResponse.badRequest()
+                .buildAndAwait()
+
+        val createdNote = noteService.createNote(note)
+
+        ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValueAndAwait(createdNote)
+    }
+
+    suspend fun updateNote(request: ServerRequest): ServerResponse = coroutineScope {
+        val note = request.awaitBodyOrNull<NoteDto>()
+            ?: return@coroutineScope ServerResponse.badRequest()
+                .buildAndAwait()
+
+        val updatedNote = noteService.updateNote(note)
+
+        ok()
+            .contentType(MediaType.APPLICATION_JSON)
+            .bodyValueAndAwait(updatedNote)
+    }
+
+    suspend fun deleteNote(request: ServerRequest): ServerResponse = coroutineScope {
+        val id = request.pathVariable("id")
+            .toLong()
+        noteService.deleteNote(id)
+
+        ServerResponse.noContent()
+            .buildAndAwait()
+    }
+}
