c9eae4441e62886baf187e0c5189a1f34877af83
Lililatortue <will.descoteaux@gmail.com>
2026-03-02 18:11:00 -0500

    added concerned service files

--- src/main/kotlin/com/mauisiios/notehub_server/service/TokenCaseStandarisation.kt
+++
@@ -1,6 +0,0 @@
-package com.mauisiios.notehub_server.service
-
-
-fun standardiseTokenCase(token: String/*TokenDto*/): String/*TokenDto*/ {
-    return token.lowercase() // TODO: Change when TokenDto is created
-}
