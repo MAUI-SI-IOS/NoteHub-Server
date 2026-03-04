64588fa48581ea45230238acc97c9eba81b98731
Lililatortue <will.descoteaux@gmail.com>
2026-03-03 21:57:36 -0500

    added repo

--- src/main/kotlin/com/mauisiios/notehub_server/data/repo/NoteTokenRepository.kt
+++ src/main/kotlin/com/mauisiios/notehub_server/data/repo/NoteTokenRepository.kt
@@ -1,4 +1,9 @@
 package com.mauisiios.notehub_server.data.repo
 
-class NoteTokenRepository {
+import com.mauisiios.notehub_server.data.entity.NoteTokenId
+import com.mauisiios.notehub_server.data.entity.NoteTokensEntity
+import com.mauisiios.notehub_server.data.entity.TokenEntity
+import org.springframework.data.repository.kotlin.CoroutineCrudRepository
+
+interface NoteTokenRepository : CoroutineCrudRepository<NoteTokensEntity,  NoteTokenId> {
 }
--- src/main/kotlin/com/mauisiios/notehub_server/data/repo/TokenRepository.kt
+++ src/main/kotlin/com/mauisiios/notehub_server/data/repo/TokenRepository.kt
@@ -1,6 +1,7 @@
 package com.mauisiios.notehub_server.data.repo
 
+import com.mauisiios.notehub_server.data.entity.TokenEntity
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 
-interface TokenRepository: CoroutineCrudRepository<TokenEntity,Word> {
+interface TokenRepository: CoroutineCrudRepository<TokenEntity,String> {
 }
