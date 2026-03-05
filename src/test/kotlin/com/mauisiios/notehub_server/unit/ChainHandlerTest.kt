1e2a8df4bbeda51ff55951be86556673beda7c81
Lililatortue <will.descoteaux@gmail.com>
2026-03-05 11:31:34 -0500

    added unit test

---
+++ src/test/kotlin/com/mauisiios/notehub_server/unit/ChainHandlerTest.kt
@@ -0,0 +1,66 @@
+package com.mauisiios.notehub_server.unit
+
+
+import com.mauisiios.notehub_server.service.COR_BUILDER.HandlerChainBuilder
+import com.mauisiios.notehub_server.service.COR_BUILDER.TaggingHandler
+import com.mauisiios.notehub_server.service.COR_BUILDER.TokenizerHandler
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import kotlin.test.Test
+
+class ChainHandlerTest{
+
+    private val taggingHandler: TaggingHandler = TaggingHandler()
+    private val tokenizerHandler: TokenizerHandler = TokenizerHandler()
+
+
+
+    @Test
+    fun `Test tokenizer transformer`() {
+        val data = "Ceci est un test C# C# 1"
+        //should return [C#,test, 1]
+        val chain = HandlerChainBuilder
+            .start(tokenizerHandler)
+            .build()
+
+        val filtered = chain.execute(data);
+        println(filtered.toString())
+        org.assertj.core.api.Assertions.assertThat(filtered)
+            .hasSize(7)
+    }
+
+    @Test
+    fun `Test tagger transformer`() {
+        val data = listOf<String>("Ceci", "est", "un", "test", "C#", "C#", "1")
+        //should return [C#,test, 1]
+        val chain = HandlerChainBuilder
+            .start(taggingHandler)
+            .build()
+
+        val filtered = chain.execute(data);
+        println(filtered.toString())
+        org.assertj.core.api.Assertions.assertThat(filtered)
+            .hasSize(3)
+    }
+
+    @Test
+    fun `Test pipe`() {
+        val data = "Ceci est un test C# C# 1"
+        //should return [C#,test, 1]
+        val chain = HandlerChainBuilder
+                    .start(tokenizerHandler)
+                    .append(taggingHandler)
+                    .build()
+
+        val filtered = chain.execute(data)
+            ?: throw Exception("filter is null")
+
+        println(filtered.toString())
+        org.assertj.core.api.Assertions.assertThat(filtered)
+            .hasSize(3)
+            .containsEntry("C#",2)
+            .containsEntry("test",1)
+            .containsEntry("1",1)
+            .doesNotContainKey("is")
+    }
+}
