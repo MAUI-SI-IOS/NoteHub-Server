629a36e8186ff603b0ea09c0567251bc720ea1c0
Lililatortue <will.descoteaux@gmail.com>
2026-03-06 12:49:40 -0500

    deleted/updated all files related to token tables

--- src/main/kotlin/com/mauisiios/notehub_server/dto/TokenDto.kt
+++
@@ -1,5 +0,0 @@
-package com.mauisiios.notehub_server.dto
-
-data class TokenDto(
-    val token: String,
-)
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -5,15 +5,10 @@ CREATE TABLE IF NOT EXISTS note (
     formatted_content TEXT NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS token (
-    token VARCHAR(255) PRIMARY KEY
-);
-
 CREATE TABLE IF NOT EXISTS note_token (
     note_id     BIGINT    NOT NULL,
     token_id    VARCHAR(255) NOT NULL,
     frequency   INTEGER     NOT NULL DEFAULT 1,
     PRIMARY KEY (note_id, token_id),
     CONSTRAINT fk_note  FOREIGN KEY (note_id)  REFERENCES note(id) ON DELETE CASCADE,
-    CONSTRAINT fk_token FOREIGN KEY (token_id) REFERENCES token(token) ON DELETE CASCADE
 );
