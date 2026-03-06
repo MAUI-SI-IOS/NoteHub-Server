eb046394362394cef12889acef1107d4ef392c75
Lililatortue <will.descoteaux@gmail.com>
2026-03-06 13:35:12 -0500

    untracked this file by accident

--- src/main/kotlin/com/mauisiios/notehub_server/mapper/NoteMapper.kt
+++ src/main/kotlin/com/mauisiios/notehub_server/mapper/NoteMapper.kt
@@ -3,7 +3,6 @@ package com.mauisiios.notehub_server.mapper
 import com.mauisiios.notehub_server.data.entity.NoteEntity
 import com.mauisiios.notehub_server.dto.NoteDto
 
-
 fun NoteDto.toEntity() = NoteEntity(
         id = this.id,
         title = this.title,
