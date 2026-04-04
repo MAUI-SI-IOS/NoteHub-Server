7f24ae6b59a49eb890e4cf156a599f014854bdc6
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-04-03 23:19:25 -0400

    Encapsulate state in InterpreterContext and expose read-only expressions
    - Restricted visibility of `expressions` and `isNewLine` to `internal`.
    - Added the `interpretedExpressions` property to provide read-only access to the internal expressions list.

--- src/main/kotlin/com/mauisiios/notehub_server/interpreter/InterpreterContext.kt
+++ src/main/kotlin/com/mauisiios/notehub_server/interpreter/InterpreterContext.kt
@@ -3,6 +3,9 @@ package com.mauisiios.notehub_server.interpreter
 import com.mauisiios.notehub_server.model.NoteFormattedExpression
 
 class InterpreterContext {
-    val expressions: MutableList<NoteFormattedExpression> = mutableListOf()
-    var isNewLine = true
+    internal val expressions: MutableList<NoteFormattedExpression> = mutableListOf()
+    internal var isNewLine = true
+
+    val interpretedExpressions: List<NoteFormattedExpression>
+        get() = expressions
 }
