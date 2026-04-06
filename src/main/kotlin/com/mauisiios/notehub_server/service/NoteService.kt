c128086693cffd87c4a64df8365479fa163a43e9
Xavier Giguere <xavier.giguere@lcieducation.net>
2026-04-06 00:05:42 -0400

    Refactor `NoteService` to integrate `MarkdownParserService` and streamline token processing with coroutines and `Flow`

--- src/main/kotlin/com/mauisiios/notehub_server/service/NoteService.kt
+++ src/main/kotlin/com/mauisiios/notehub_server/service/NoteService.kt
@@ -8,15 +8,19 @@ import com.mauisiios.notehub_server.dto.NoteDto
 import com.mauisiios.notehub_server.mapper.toDto
 import com.mauisiios.notehub_server.mapper.toEntity
 import com.mauisiios.notehub_server.service.COR_BUILDER.TokenizerFactory
-import kotlinx.coroutines.*
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import org.springframework.stereotype.Service
 
 @Service
 class NoteService(
     private val noteRepository: NoteRepository,
-    private val noteTokenRepository: NoteTokenRepository
+    private val noteTokenRepository: NoteTokenRepository,
+    private val markdownParserService: MarkdownParserService
 ) {
     fun getAll(): Flow<NoteDto> = noteRepository.findAll()
         .map(NoteEntity::toDto)
@@ -27,39 +31,45 @@ class NoteService(
     suspend fun getByTitle(title: String): NoteDto? = noteRepository.findByTitle(title)
         ?.toDto()
 
-    suspend fun createNote(note: NoteDto): NoteDto = withContext(Dispatchers.IO) { // Optimise les routines pour les requete reseau/db
-        val savedNoteDtoJob = async {
-            noteRepository.save(note.toEntity())
-                .toDto()
+    suspend fun createNote(note: NoteDto): NoteDto = coroutineScope { // Optimise les routines pour les requete reseau/db
+        val tokenProcessingFlow = TokenizerFactory.tokenize(note.rawContent)
+            ?: throw Exception("no tokens???") // TODO: change for a specific Exception for better handling
+
+        var contentToFormat = note.rawContent
+        tokenProcessingFlow.collect { (token, _) ->
+            // TODO: Set a threshold so that only recurring theme are replaced by links
+            contentToFormat = contentToFormat.replace(token, "[$token](#${token})") // Replacing tokens by links to them 
         }
 
-        val tokenProcessingJob = async {
-            TokenizerFactory
-                .tokenize(note.rawContent)
+        var parsedContent = markdownParserService.parse(contentToFormat)
+
+        val savedNoteDtoJob = async {
+            noteRepository.save(
+                note.toEntity()
+                    /* NOTE: will be commented out until the conversion is implemented
+                    .apply { formattedContent = parsedContent.joinToString(separator = "") { 
+                        it.content
+                    } } // TODO: should be a JSON object
+                     */
+            ).toDto()
         }
 
         // attendre que la note soit sauvegardé et que les tokens soient processé
         // avant dexecuter le mapping
         val savedNoteDto = savedNoteDtoJob.await()
-        val processedTokensMap = tokenProcessingJob.await()
 
 
-        val noteTokens = processedTokensMap?.map { (key, value) ->
+        val noteTokens = tokenProcessingFlow.map { (key, value) ->
             NoteTokenEntity(
                 savedNoteDto.id ?: throw Exception("no id???") , // TODO: Handle with a custom exception
                 key,
                 value
             )
-        } ?: throw Exception("no tokens???") // TODO: Handle with a custom exception
-
-
-        coroutineScope {
-            // coroutineScope block l'execution de la fonction jusqu'a ce que tous les childJobs
-            // sont completer
-            launch { noteTokenRepository.saveAll(noteTokens).collect {  } }
         }
 
-        return@withContext savedNoteDto
+        launch { noteTokenRepository.saveAll(noteTokens).count() }
+
+        return@coroutineScope savedNoteDto
     }
 
     suspend fun deleteNote(id: Long) = noteRepository.deleteById(id)
